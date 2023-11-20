306f3539a8546fa35b922fe77285827e71425347
Luiz Verissimo <luizverissimosouza@gmail.com>
2023-11-20 14:43:18 -0300

    sandbox logic

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0;
+}
+
+body {
+  background: #ebe3d5;
+  color: #22092c;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+border-style,
+input,
+button {
+  font-family: "Roboto Slab", serif;
+  font-size: 16px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+strong {
+  font-weight: 500;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+button {
+  cursor: pointer;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+// import TrackerManagment from "../views/TrackerManagement.vue";
+import SandBox from "../views/SandBox.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: SandBox,
   },
 ];
 
